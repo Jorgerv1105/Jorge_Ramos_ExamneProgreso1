c5c6cae53162e150156e6b5f2b644168b9b2706f
Jorge <Jorge@DESKTOP-AQ0N3LG>
2025-04-28 07:10:41 -0500

    Agregar archivos de proyecto.

---
+++ Jorge_Ramos_ExamneProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jorge_Ramos_ExamneProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
